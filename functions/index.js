8968ecfb7b557591de8bd8f62bfc13841d287be9
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-03-11 12:05:26 +0200

    Add admin mode

--- functions/index.js
+++ functions/index.js
@@ -1,3 +1,4 @@
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const create_user = require('./create_user');
 
@@ -7,6 +8,11 @@ const create_user = require('./create_user');
 // exports.helloWorld = functions.https.onRequest((request, response) => {
 //  response.send("Hello from Firebase!");
 // });
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://one-time-password-6f6b1.firebaseio.com"
+  });
 
 exports.create_user = functions.https.onRequest(create_user);
 
