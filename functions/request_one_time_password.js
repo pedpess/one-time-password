eec90ead4cc634aaa464e4674f261d44db6dd5e6
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-03-13 02:05:32 +0200

    Refactor request twilio API func

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -2,32 +2,30 @@ const admin = require('firebase-admin');
 const twilio = require('./twilio');
 
 module.exports = function(req, res) {
-    if (!req.body.phone) {
-        return res.status(422).send({ error: 'You must provide a phone number '});
-    }
+  if (!req.body.phone) {
+    return res.status(423).send({ error: 'You must provide a phone number' });
+  }
 
-    const phone = String(req.body.phone).replace(/[^\d]/g, "");
+  const phone = String(req.body.phone).replace(/[^\d]/g, '');
 
-    return admin.auth().getUser(phone)
-        .then(userRecord => {
-            const code = Math.floor((Math.random() * 8999 + 1000));
+  admin.auth().getUser(phone)
+    .then(userRecord => {
+      const code = Math.floor((Math.random() * 8999 + 1000));
 
-            twilio.messages.create({
-                body: `Your code is ${code}`,
-                to: phone,
-                from: '+13602180968',
-            }, (error) => {
-                if (error) {
-                    return res.status(422).send(error);
-                }
+      twilio.messages.create({
+        body: 'Your code is ' + code,
+        to: phone,
+        from: '+13602180968'
+      }, (err) => {
+        if (err) { return res.status(422).send(err + ' :Twilio API Error'); }
 
-                admin.database().ref('users/' + phone)
-                    .update({ code: code, codeValid: true }, () => {
-                        res.send({ success: true });
-                    });
-            })
-        })
-        .catch(error => {
-            res.status(422).send({ error });
-        });
+        admin.database().ref('users/' + phone)
+          .update({ code: code, codeValid: true }, () => {
+            res.send({ success: true });
+          });
+      })
+    })
+    .catch((err) => {
+      res.status(424).send(err + ' :Error getting user');
+    });
 }
