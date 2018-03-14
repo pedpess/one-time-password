9a5fb2b53408e8827a781e505e1f85170023af7b
Pedro Pessoa <pedro.pessoa@futurice.com>
2018-03-15 01:18:52 +0200

    Add plus sign to stop damn bug in Twilio

--- functions/request_one_time_password.js
+++ functions/request_one_time_password.js
@@ -3,7 +3,7 @@ const twilio = require('./twilio');
 
 module.exports = function(req, res) {
   if (!req.body.phone) {
-    return res.status(423).send({ error: 'You must provide a phone number' });
+    return res.status(422).send({ error: 'You must provide a phone number' });
   }
 
   const phone = String(req.body.phone).replace(/[^\d]/g, '');
@@ -14,7 +14,7 @@ module.exports = function(req, res) {
 
       twilio.messages.create({
         body: 'Your code is ' + code,
-        to: phone,
+        to: '+' + phone,
         from: '+13602180968'
       }, (err) => {
         if (err) { return res.status(422).send(err + ' :Twilio API Error'); }
@@ -26,6 +26,6 @@ module.exports = function(req, res) {
       })
     })
     .catch((err) => {
-      res.status(424).send(err + ' :Error getting user');
+      res.status(422).send(err + ' :Error getting user');
     });
 }
